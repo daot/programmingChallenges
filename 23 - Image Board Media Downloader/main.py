5c7df4535af2aff1278538e30f468a7252e67a49
toad <47069617+toadsucks@users.noreply.github.com>
2019-02-06 22:57:50 -0600

    fixed print bug and added thread subject to folder name

--- 23 - Image Board Media Downloader/main.py
+++ 23 - Image Board Media Downloader/main.py
@@ -5,11 +5,13 @@ while(True):
     count = 1
     while (count < len(sys.argv)):
         url = str(sys.argv[count]).replace('http://', '').replace('https://', '')
-        print(datetime.datetime.now(),'Checking:', url)
 
         data = requests.get(f'https://{url}')
 
         soup = BeautifulSoup(data.text, 'html.parser')
+		
+        sys.stdout.write(f'{datetime.datetime.now()} Checking: {soup.find("span", {"class": "subject"}).text} - {url}\n')
+        sys.stdout.flush()
 
         imgs = []
         for i in soup.find_all('a', {'class': 'fileThumb'}, href=True):
@@ -20,14 +22,14 @@ while(True):
             for x in i.find_all('a'):
                 names.append(x.text.replace("(...)", ""))
 
-        if not os.path.exists(f'downloads/{url.split("/")[1]} - {url.split("/")[3]}'):
-            os.makedirs(f'downloads/{url.split("/")[1]} - {url.split("/")[3]}')
+        if not os.path.exists(f'downloads/{soup.find("span", {"class": "subject"}).text} - {url.split("/")[1]} - {url.split("/")[3]}'):
+            os.makedirs(f'downloads/{soup.find("span", {"class": "subject"}).text} - {url.split("/")[1]} - {url.split("/")[3]}')
 
         for i in range(0, len(imgs)):
-            if(os.path.isfile(f'downloads/{url.split("/")[1]} - {url.split("/")[3]}/{names[i]}') == False):
-                print(datetime.datetime.now(), 'Downloading:', names[i], end=' - ')
-                urllib.request.urlretrieve(f'https://{imgs[i]}', f'downloads/{url.split("/")[1]} - {url.split("/")[3]}/{names[i]}')
-                print('Done')
+            if(os.path.isfile(f'downloads/{soup.find("span", {"class": "subject"}).text} - {url.split("/")[1]} - {url.split("/")[3]}/{names[i]}') == False):
+                sys.stdout.write(f'{datetime.datetime.now()} Downloading: {names[i]}\n')
+                sys.stdout.flush()
+                urllib.request.urlretrieve(f'https://{imgs[i]}', f'downloads/{soup.find("span", {"class": "subject"}).text} - {url.split("/")[1]} - {url.split("/")[3]}/{names[i]}')
         count += 1
 
     time.sleep(10)
